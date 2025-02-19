4ce6cede60ef50f9f6050278fccb4621b76d53bb
Eduard <eduardc@runninghill.co.za>
2025-02-19 17:58:18 +0200

    ios implementation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -35,6 +35,7 @@ class _HomeScreenState extends State<HomeScreen> {
         onlyAlertOnce: true,
       ),
       iosNotificationOptions: const IOSNotificationOptions(
+        //TODO Show notification is still set to false here
         showNotification: false,
         playSound: false,
       ),
